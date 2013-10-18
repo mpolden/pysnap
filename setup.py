a82220efe4a90d99a59034b1a3ba950ffd5438ac
Martin Polden <martin.polden@gmail.com>
2013-10-18 19:41:54 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='pysnap',
+    version='0.1',
+    description='Snapchat API client in Python',
+    long_description=open('README.md').read(),
+    author='Martin Polden',
+    author_email='martin.polden@gmail.com',
+    url='https://github.com/martinp/pysnap',
+    packages=['pysnap'],
+    scripts=['bin/get_snaps.py'],
+    install_requires=[
+        'docopt>=0.6',
+        'pycrypto>=2.6',
+        'requests>=2.0'
+    ],
+    license=open('LICENSE').read()
+)
